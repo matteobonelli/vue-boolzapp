0214dd9b4aefd3dc64fbb6fbfcd5357260293959
matteobonelli <matteobonelli08@gmail.com>
2023-11-03 00:27:20 +0100

    milestone 3 completed

--- js/script.js
+++ js/script.js
@@ -6,11 +6,41 @@ createApp({
         return {
             contacts : contactList,
             activeContact : '',
+            newMessage : ''
         }
     },
     methods : {
-       
-        
+       addMessage(){
+        this.contacts.forEach(element => {
+            if(this.activeContact === element.name && this.newMessage !== ''){
+                const newMsg = {
+                    date : '03/11/2023 00:06:01',
+                    message : this.newMessage,
+                    status : 'sent'
+                }
+                element.messages.push(newMsg)
+                this.newMessage = ''
+                setTimeout(() =>{
+                    const answer = {
+                        date : '03/11/2023 00:06:02',
+                        message : 'ok',
+                        status : 'received'
+                    }
+                    return element.messages.push(answer)
+                   }, 1000)
+            }
+            
+        });
+       },
+       userAnswer(element){
+        const newMsg = {
+            date : '03/11/2023 00:06:02',
+            message : 'ok',
+            status : 'received'
+        }
+        element.messages.push(newMsg)
+       }
+               
     },
     computed : {
         
